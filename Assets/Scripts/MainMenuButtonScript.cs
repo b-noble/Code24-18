1640c0e682180d90b4e6dae6f357e99ebdc1b3da
b-noble <35877919+b-noble@users.noreply.github.com>
2018-01-27 15:35:54 -0700

    Add files via upload

---
+++ Assets/Scripts/MainMenuButtonScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonScript : MonoBehaviour {
+
+	public Button eat;
+	public Button task;
+	public Button games;
+	public Button shop;
+	public Button friends;
+	public Button info;
+
+	public Text playerText;
+
+	void Start () 
+	{
+		eat.onClick.AddListener (EatOnClick);
+		task.onClick.AddListener (TaskOnClick);
+		games.onClick.AddListener (GamesOnClick);
+		shop.onClick.AddListener (ShopOnClick);
+		friends.onClick.AddListener (FriendsOnClick);
+		info.onClick.AddListener (InfoOnClick);
+
+		playerText.text = PlayerPrefs.GetString("Name") + "\n" + PlayerPrefs.GetString("Age");
+	}
+
+	void EatOnClick ()
+	{
+		SceneManager.LoadScene ("EatScene", LoadSceneMode.Single); 
+	}
+
+	void TaskOnClick ()
+	{
+		SceneManager.LoadScene ("TaskScene", LoadSceneMode.Single); 
+	}
+
+	void GamesOnClick ()
+	{
+		SceneManager.LoadScene ("GameScene", LoadSceneMode.Single); 
+	}
+
+	void ShopOnClick ()
+	{
+		SceneManager.LoadScene ("ShopScene", LoadSceneMode.Single); 
+	}
+
+	void FriendsOnClick ()
+	{
+		SceneManager.LoadScene ("FriendScene", LoadSceneMode.Single); 
+	}
+
+	void InfoOnClick ()
+	{
+		SceneManager.LoadScene ("InfoScene", LoadSceneMode.Single);
+	}
+}
